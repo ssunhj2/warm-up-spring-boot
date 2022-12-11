f1561dba59bf0f30935da28d4fcfe6bc77129350
ssunhj2 <wiiro017@gmail.com>
2022-12-11 23:40:06 +0900

    임시로 작성한 repository 대신 JDBC repository로 변경한다.

--- src/main/java/com/xun/warmup/SpringConfig.java
+++ src/main/java/com/xun/warmup/SpringConfig.java
@@ -1,13 +1,22 @@
 package com.xun.warmup;
 
+import com.xun.warmup.repository.JdbcMemberRepository;
 import com.xun.warmup.repository.MemberRepository;
 import com.xun.warmup.repository.MemoryMemberRepository;
 import com.xun.warmup.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -16,7 +25,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
     
 }
